f77d8083ff59614cf0acefd34728898f59da9dea
wijdan1995 <wijdan.w.k@outlook.sa>
2019-04-27 13:18:30 +0300

    Add the other lines and if statment

--- mbta.js
+++ mbta.js
@@ -1,6 +1,13 @@
 const stopsBetweenStations = function (startLine, startStation, endLine, endStation) {
-    let red = ['south station', 'park street', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife']
+    let lines = {
+        Red: ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+        Green: ['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore'],
+        Orange: ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
+    }
+
     if (startLine === endLine) {
-        return red.indexOf(endStation) - red.indexOf(startStation)
+        return Math.abs(lines[endLine].indexOf(endStation) - lines[startLine].indexOf(startStation))
+    } else {
+        return Math.abs(lines[endLine].indexOf(endStation) + lines[startLine].indexOf('Park Street'))
     }
 }
