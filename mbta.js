c5de305e926638826baf98338609161891562edf
hamoghamdi <haneen.aljabrah@gmail.com>
2019-04-27 12:14:30 +0300

    this one works two-ways on oneline, one way on different lines only. i don't know what i'm doing wrong, i want to have it work both ways/directions on differnt lines, with the hint of 'Park Street', the code looks perfect in reading but it won't work on console the line where console says it has a problem works fine when i try it outside of the function

--- mbta.js
+++ mbta.js
@@ -153,3 +153,64 @@ stopsBetweenStations('Red', 'South Station', 'Red', 'Alewife') // 7 stops
 stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stops
 
 */
+
+//========================= another try out =========================//
+
+const red = ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"];
+const green = ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"]
+const orange = ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"];
+const subway = [red, green, orange];
+
+const stopsBetweenStations = function (startLine, startStation, endLine, endStation) {
+  switch (startLine) {
+    case 'Red': startLine = 0;
+      break;
+    case 'Green': startline = 1;
+      break;
+    case 'Orange': srartLine = 2;
+      break;
+  }
+  switch (endLine) {
+    case 'Red': endLine = 0;
+      break;
+    case 'Green': endLine = 1;
+      break;
+    case 'Orange': endLine = 2;
+      break;
+  }
+
+  if (startLine === endLine) { // this works two-ways on one line 
+    return Math.abs(subway[startLine].indexOf(startStation) - subway[endLine].indexOf(endStation)) + " stops."// math.abs()
+  } else {
+
+    // to determine if it is from [0] or from the last index => compare it to 'park street' index if bigger then its from end if smaller then it's from up
+    // when = park street but on different line => issue !!!!
+    let tmp = Math.abs(subway[startLine].indexOf(startStation) - subway[startLine].indexOf('Park Street')); // this line has a problem 
+    if (tmp > 0) {
+      return subway[startLine].indexOf(startStation) - subway[startLine].indexOf('Park Street') + subway[endLine].indexOf(endStation) - subway[startLine].indexOf('Park Street');
+    } else {
+      // if (startStation === 'Park Street' || endStation === 'Park Street')
+        if (startStation === 'Park Street') {
+          return subway[startLine].indexOf('Park Street') + subway[endLine].indexOf(endStation);
+        } else { // this one works
+      if (endStation === 'Park Street') {
+        return subway[startLine].indexOf(startStation) - subway[endLine].indexOf('Park Street');
+      } else
+        return Math.abs(subway[startLine].indexOf(startStation) - subway[startLine].indexOf('Park Street')) + Math.abs(subway[endLine].indexOf('Park Street') - subway[endLine].indexOf(endStation)) + " stops";
+    } // this line works one way top-bottom on different lines
+  }
+}
+
+}
+/* 
+stopsBetweenStations('Green', 'Kenmore', 'Red', 'South Station') // 6 stops
+VM921:33 Uncaught TypeError: Cannot read property 'indexOf' of undefined
+    at stopsBetweenStations (<anonymous>:33:42)
+    at <anonymous>:1:1
+
+stopsBetweenStations('Green', 'Copley', 'Red', 'Park Street') // 7 stops
+
+VM921:33 Uncaught TypeError: Cannot read property 'indexOf' of undefined
+    at stopsBetweenStations (<anonymous>:33:42)
+    at <anonymous>:1:1
+    */
