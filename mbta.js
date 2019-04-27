77cd075bff03295fdf1de213de5b8ffb44853d28
somayahbahkeem <iove36@hotmail.com>
2019-04-28 00:30:42 +0300

    I try to salve but not finsh yet

---
+++ mbta.js
@@ -0,0 +1,40 @@
+
+
+  //------------------------------------------------
+const  Red =["South Station", "Park Street" , "Kendall" ,"Central", "Harvard" ,"Porter" ,"Davis" ,"Alewife"]
+const Green= ["Government Center", "Park Street" ," Boylston", "Arlington", "Copley", " Hynes" ,"Kenmore"]
+const Orange= ["North Station", "Haymarket",  "Park Street" ,"State" ,"Downtown Crossing" ,"Chinatown" ,"Back Bay" ,"Forest Hills"]
+        
+
+ const  stopsBetweenStations=function(StartLine, StartSation, EndLine,EndStation){
+
+
+        for(let i=0;i>= ;i++){
+
+        }
+        
+ };
+ 
+ stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife') // 0 stops
+ stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station') // 7 stops
+ stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stops
+
+                  
+
+
+
+
+
+
+
+
+  
+  
+ 
+  
+  
+ 
+  
+
+
+  
