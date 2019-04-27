56d905d1cc1b477901f32b42eff946ddc10aa66c
taha-sindi <taha.sindi@gmail.com>
2019-04-28 01:04:08 +0300

    zzzzzzzz

--- MBTA.js
+++ MBTA.js
@@ -1,216 +1,37 @@
-let red = [{
-    name:"South Station",
-    order:1
-},{
-    name:"Park Street",
-    order:2
-},{
-    name:"Kendall",
-    order:3
-},{
-    name:"Central",
-    order:4
-},{
-    name:"Harvard",
-    order:5
-},{
-    name:"Porter",
-    order:6
-},{
-    name:"Davis",
-    order:7
-},{
-    name:"Alewife",
-    order:8
-}];
-//red array
-//====================================================================
-let orange = [{
-    name:"North Station",
-    order:1
-},{
-    name:"Haymarket",
-    order:2
-},{
-    name:"Park Street",
-    order:3
-},{
-    name:"State",
-    order:4
-},{
-    name:"Downtown Crossing",
-    order:5
-},{
-    name:"Chinatown",
-    order:6
-},{
-    name:"Back Bay",
-    order:7
-},{
-    name:"Forest Hills",
-    order:8
-}];
-//orange array
-//====================================================================
-let green = [{
-    name:"Government Center",
-    order:1
-},{
-    name:"Park Street",
-    order:2
-},{
-    name:"Boylston",
-    order:3
-},{
-    name:"Arlington",
-    order:4
-},{
-    name:"Copley",
-    order:5
-},{
-    name:"Hynes",
-    order:6
-},{
-    name:"Kenmore",
-    order:7
-}];
-//green array
-//====================================================================
-const stopsBetweenStations=function(colorStart,start,colorEnd,end){
-    //============================================
-    let startOrder;
-    let endOrder;
-    let stops=-1;
-    let stations=[];
-    //============================================
-    //============================================
-    if(colorStart.toLowerCase()==="red"){
-        for(i=0;i<red.length;i++){
-            if(start.toLowerCase()===red[i].name.toLowerCase()){
-            startOrder=red[i].order;
-            break;
-            }
-        }
-    }
-    if(colorEnd.toLowerCase()==='red'){
-        for(i=0;i<red.length;i++){
-            if(end.toLowerCase()===red[i].name.toLowerCase()){
-                endOrder=red[i].order;
-                break;
-            }
-        }
-    }
-    if(colorStart.toLowerCase()==="green"){
-        for(i=0;i<green.length;i++){
-            if(start.toLowerCase()===green[i].name.toLowerCase()){
-            startOrder=green[i].order;
-            break;
-            }
-        }
-    }
-    if(colorEnd.toLowerCase()==='green'){
-        for(i=0;i<green.length;i++){
-            if(end.toLowerCase()===green[i].name.toLowerCase()){
-                endOrder=green[i].order;
-                break;
-            }
-        }
-    }
-    if(colorStart.toLowerCase()==="orange"){
-        for(i=0;i<orange.length;i++){
-            if(start.toLowerCase()===orange[i].name.toLowerCase()){
-            startOrder=orange[i].order;
-            break;
-            }
-        }
-    }
-    if(colorEnd.toLowerCase()==='orange'){
-        for(i=0;i<orange.length;i++){
-            if(end.toLowerCase()===orange[i].name.toLowerCase()){
-                endOrder=orange[i].order;
-                break;
-            }
-        }
-    }
-    //============================================
-    //============================================
-    if((colorStart.toLowerCase()==='red')&&(colorEnd.toLowerCase()==='red')){
-        if(startOrder<endOrder){
-            for(i=startOrder-1;i<=endOrder;i++){
-                stations.push(red[i].name);
-                if(stops===0){
-                    console.log('Rider boards the train a Red Line and '+red[startOrder-1].name);
-                }
-                else if(stops>0){
-                    console.log('Rider arrives at Red Line and '+red[startOrder-1].name);
-                }
-                stops++;
-            }
-        }
-        else if(startOrder>endOrder){
-            for(i=startOrder-1;i>=endOrder;i--){
-                stations.push(red[i].name);
-                if(stops===0){
-                console.log('Rider boards the train a Red Line and '+red[startOrder-1].name);
-            }
-            else if(stops>0){
-                console.log('Rider arrives at Red Line and '+red[startOrder-1].name);
-            }
-                stops++;
-            }
-        }
-        console.log('Rider exits the train at Red Line and '+red[endOrder-1].name);
-        console.log(stops+' stops');
-    }
-    //============================================
-    //============================================
-    if((colorStart.toLowerCase()==='green')&&(colorEnd.toLowerCase()==='green')){
-        if(startOrder<endOrder){
-            for(startOrder;startOrder<=endOrder;startOrder++){
-                stations.push(green[startOrder-1].name);
-                stops++;
-            }
-        }
-        else if(startOrder>endOrder){
-            for(startOrder;startOrder>=endOrder;startOrder--){
-                stations.push(green[startOrder-1].name);
-                if(stops===0){
-                console.log('Rider boards the train a Green Line and '+green[startOrder-1].name);
-            }
-            else if(stops>0){
-                console.log('Rider arrives at Green Line and '+green[startOrder-1].name);
-            }
-                stops++;
-            }
-        }
-        console.log('Rider exits the train at Green Line and '+green[endOrder].name);
-        console.log(stops+' stops');
-    }
-    //============================================
-    //============================================
-    if((colorStart.toLowerCase()==='orange')&&(colorEnd.toLowerCase()==='orange')){
-        if(startOrder<endOrder){
-            for(startOrder;startOrder<=endOrder;startOrder++){
-                stations.push(orange[startOrder-1].name);
-                stops++;
-            }
-        }
-        else if(startOrder>endOrder){
-            for(startOrder;startOrder>=endOrder;startOrder--){
-                stations.push(orange[startOrder-1].name);
-                if(stops===0){
-                console.log('Rider boards the train a Orange Line and '+orange[startOrder-1].name);
-            }
-            else if(stops>0){
-                console.log('Rider arrives at Orange Line and '+orange[startOrder-1].name);
-            }
-                stops++;
-            }
-        }
-        console.log('Rider exits the train at Orange Line and '+orange[endOrder].name);
-        console.log(stops+' stops');
-    }
-    //============================================
-    //============================================
+const mbta = [ //================================subway==================================
+
+{ orange:["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]},
     
-}
+{ green:["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"]},
+
+{ red:["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"]},]
+    
+const bustop = function (initline,initstation,lastline,laststation) 
+    
+{ let i,j;if(initline === "red") {i=0;}
+    
+else if (initline === "green") {i=1;}
+    
+else if (initline === "orange" ) {i=2;}
+    
+if (initline === lastline)
+    
+{return Math.abs(mbta[i][initline].indexOf(initstation)
+    
+- mbta[i][lastline].indexOf(laststation));}
+    
+else if (lastline === "red") {j=0;}
+    
+else if (lastline === "green") {j=1;}
+    
+else if (lastline === "orange" ) {j=2;}
+    
+return Math.abs(mbta[i][initline].indexOf(initstation)
+
+- mbta[i][initline].indexOf("Park Street"))
+    
++ Math.abs(mbta[j][lastline].indexOf(laststation)
+
+- mbta[j][lastline].indexOf("Park Street"));} 
+    
+// mbta("initline","initstation","lastline","laststation")
