8b919a4e8751bf514340da6fd8c45f545d38db16
asmamk <i.amk94@hotmail.com>
2019-04-28 11:05:59 +0300

    i try to solve

---
+++ mbta.js
@@ -0,0 +1,33 @@
+const station = 
+{
+       red :['South Station',
+            'Park Street',
+            'Kendall',
+            'Central',
+            'Harvard',
+            'Porter',
+            'Davis',
+            'Alewife'], 
+
+      green : ['Government Center',
+                'Park Street',
+                 'Boylston',
+                'Arlington',
+                'Copley',
+                'Hynes',
+                'Kenmore'],
+
+	orang :[ ' North Station',
+             'Haymarket',
+             'Park Street',
+             'State',
+             'Downtown Crossing',
+             'Chinatown',
+             'Back Bay',
+             'Forest Hills'] ,
+
+	stopBetweenStation : function(startLine ,startStation ,endLine 		,endStation)
+
+		if( sation.red[1] && station.green[1] && station.orange[2] === 'Park Street' ) {
+return 'Park Street' ;
+}
