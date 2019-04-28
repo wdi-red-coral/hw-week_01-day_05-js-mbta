f5f3b1c36e6f3b638cbee73975bbda3a461e392a
somayahbahkeem <iove36@hotmail.com>
2019-04-28 10:49:29 +0300

    the h.w done

--- mbta.js
+++ mbta.js
@@ -1,4 +1,49 @@
+const subwayLine ={
+  Red :["South Station",
+ "Park Street" ,
+  "Kendall" ,
+  "Central", "Harvard" ,
+  "Porter" ,
+  "Davis" ,
+  "Alewife"
+],
+Green:[
+  "Government Center",
+ "Park Street" ,
+ " Boylston",
+  "Arlington", 
+  "Copley", 
+  " Hynes" ,
+  "Kenmore"
+],
+Orange: [
+  "North Station",
+ "Haymarket",
+   "Park Street" ,
+   "State" ,
+   "Downtown Crossing" ,
+   "Chinatown" ,
+   "Back Bay" ,
+   "Forest Hills"]
+};
+const  stopsBetweenStations=function(StartLine, StartSation, EndLine,EndStation){
+  const startStationIndex= subwayLine[StartLine].indexOf(StartSation);
+  const endStationIndex= subwayLine[endLine].indexOf(endSation);
+  if(StartLine===endLine){
+    return  Math.abs(startStationIndex-endStationIndex);
+  }
+  const startLineParkStreetIndex = subwayLine[StartLine].indexOf("park street");
+  //get the index of the endLine park street station 
+  const tripToParkStreet =Math.abs(startStationIndex-startLineParkStreetIndex);
 
+  const endLineParkStreetIndex =subwayLine[endLine].indexOf("park street");
+
+  const tripToDesnation=Math.abs(endStationIndex-endLineParkStreetIndex);
+
+  const totalTrip = tripToDesnation+tripToParkStreet;
+  return totalTrip;
+
+}
 
   //------------------------------------------------
 const  Red =["South Station", "Park Street" , "Kendall" ,"Central", "Harvard" ,"Porter" ,"Davis" ,"Alewife"]
