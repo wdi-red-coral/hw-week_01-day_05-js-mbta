793c00cea05a4d65989829b82b82ac015723d666
omar4 <tarawri.dev@gmail.com>
2019-04-28 09:35:46 +0300

    howework

---
+++ mbta.js
@@ -0,0 +1,33 @@
+// homework 5
+
+// Red
+const red = function(tName,offStation,onStation)
+{
+  
+  return tName+ ( onStation -  offStation- 1);
+}
+
+const res = red("Red ", 1,6);
+console.log(res +" stops");
+
+// Green
+const green = function(tName,offStation,onStation)
+{
+  
+  return tName+ ( onStation -  offStation- 1);
+}
+
+const resg = green("Green ", 4,7);
+console.log(resg +" stops");
+
+
+// Orange
+
+const orange = function(tName,offStation,onStation)
+{
+  
+  return tName+ ( onStation -  offStation- 1);
+}
+
+const reso = green("Orange ", 1,9);
+console.log(reso +" stops");
