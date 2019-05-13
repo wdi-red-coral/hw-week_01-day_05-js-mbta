49eca601e015f648bb6ebd0ed7ad91482b4aa054
oalmalky5 <oalmalky5@gmail.com>
2019-05-13 07:08:23 +0300

    updated

--- mbta.js
+++ mbta.js
@@ -1,52 +1,40 @@
-const subwayLines = {
-    Red:[
-      'South Station',
-      'Park Street',
-      'Kendall',
-      'Central',
-      'Harvard',
-      'Porter',
-      'Davis',
-      'Alewife'
-    ],
-    Green:[
-      'Government Center',
-      'Park Street',
-      'Boylston',
-      'Arlington',
-      'Copley',
-      'Hynes',
-      'Kenmore']
-    ,  
-
-
-
-    Orange:['North Station',
-      'Haymarket',
-      'Park Street',
-      'State',
-      'Downtown Crossing',
-      'Chinatown',
-      'Back Bay',
-      'Forest Hills'],
+const sLines = {
+  Red: [
+    "South Station",
+    "Park Street",
+    "Kendall",
+    "Central",
+    "Harvard",
+    "Porter",
+    "Davis",
+    "Alewife"
+   ],
+  Green: ["Government Center",
+    "Park Street",
+    "Bolyston",
+    "Arlington",
+    "Copley",
+    "Hynes",
+    "Kenmore"
+  ],
+  Orange: [
+    "North Station",
+    "Haymarket",
+    "Park Street",
+    "State",
+    "Downtown Crossing",
+    "Chinatown",
+    "Back Bay",
+    "Forest Hills"
+  ]
 };
 
-let stopsBetweenStations = function(startLine, startStation, endLine, endStation){
-    const startStationIndex = subwayLines[startLine].indexOf(startStation);
-    const endStationIndex = subwayLines[endLine].indexOf(endStation);
-    
-    if(startLine === endLine){
-        return Math.abs(startStationIndex - endStationIndex);
-    }
+ let stopsOnOneLine = function(line, start, end) {
+  let stops = sLines[line].indexOf(start) - sLines[line].indexOf(end);
+  return stops
+};
 
-    const startLineParkStreetIndex = subwayLines[startLine].indexOf('Park Street');
-    const tripToParkStreet = Math.abs(startStationIndex - startLineParkStreetIndex);
-    const endLineParkStreetIndex = subwayLines[endLine].indexOf('Park Street');
-    const tripToDestination = Math.abs(endStationIndex - endLineParkStreetIndex);
-    const totalTrip = tripToParkStreet + tripToDestination ;
-
-    return totalTrip;
-
-    
-}
-
+ let stopsBetweenStations = function(startLine, startStation, endLine, endStation) {
+  if (startLine === endLine) {
+    stops = stopsOnOneLine(startLine, startStation, endStation);
+  } 
